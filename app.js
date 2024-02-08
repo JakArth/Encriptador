2e8a30602f14b231591bd6307fd7c6eb0aa20dd9
JakArth <146471928+JakArth@users.noreply.github.com>
2024-02-08 16:34:59 -0500

    minusculas y acentos

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ let texArea = document.getElementById("respuesta").style.display="none";
 function Encriptado() {
     let frase = [];
     let texto = document.getElementById("txtArea").value;
+    let txtMinuscula = texto.toLowerCase();
+    texto = txtMinuscula;
     for (let i = 0; i < texto.length; i++) frase.push(texto[i]);
-    console.log(frase);
-    for (let i = 0; i < frase.length; i++)
-        if (frase[i] === "a") {
+    
+    for (let i = 0; i < texto.length; i++)
+        if (texto[i] === "a" || texto[i] === "á") {
             frase.splice(i, 1, "ai");
-        } else if (frase[i] === "e") {
+        } else if (texto[i] === "e" || texto[i] === "é") {
             frase.splice(i, 1, "enter");
-        } else if (frase[i] === "i") {
+        } else if (texto[i] === "i" || texto[i] === "í") {
             frase.splice(i, 1, "imes");
-        } else if (frase[i] === "o") {
+        } else if (texto[i] === "o" || texto[i] === "ó") {
             frase.splice(i, 1, "ober");
-        } else if (frase[i] === "u") {
+        } else if (texto[i] === "u" || texto[i] === "ú") {
             frase.splice(i, 1, "ufat");
         }
     document.getElementById("img3").style.display="none";
